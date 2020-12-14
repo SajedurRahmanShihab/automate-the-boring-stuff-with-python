2d05f936ae23c64c3a01dcfca8880e98b96a3935
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2020-12-15 03:38:32 +0600

    Basic operator in python

---
+++ PART1_PYTHON_PROGRAMMING_BASICS/PythonBasics.py
@@ -0,0 +1,8 @@
+# Operators
+print("This is the operation of an EXPONENT operator", 2**3)
+print("This is the operation of a MODULAS/REMAINDER operator", 22 % 8)
+print("This the operation of an INTEGER DIVISION/FLOORED DIVISION operator", 22//8)
+print("This the operation of an DIVISION operator", 22/8)
+print("This the operation of an MULTIPLICATION operator", 3*5)
+print("This the operation of an SUBTRACTION operator", 5-2)
+print("This the operation of an ADDITION operator", 2+2)
