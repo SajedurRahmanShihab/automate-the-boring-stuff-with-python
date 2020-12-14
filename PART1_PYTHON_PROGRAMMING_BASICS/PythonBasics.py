1d9eab86f31fb07a74736f0ffbc3b7486e42201d
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2020-12-15 04:00:52 +0600

    pthon basics chapter completed

--- PART1_PYTHON_PROGRAMMING_BASICS/PythonBasics.py
+++ PART1_PYTHON_PROGRAMMING_BASICS/PythonBasics.py
@@ -6,3 +6,29 @@ print("This the operation of an DIVISION operator", 22/8)
 print("This the operation of an MULTIPLICATION operator", 3*5)
 print("This the operation of an SUBTRACTION operator", 5-2)
 print("This the operation of an ADDITION operator", 2+2)
+
+
+# String Concatenation and Replication
+print("Alice" + "Bob")
+print("Alice " * 5)
+
+# Storing Values in Variables
+spam = 40
+print(spam)
+eggs = 2
+print(spam + eggs)
+print(spam + eggs + spam)
+spam = spam + 2
+print(spam)
+
+
+# Your First Program
+# This program says hello and asks for my name
+print("HEllo, world!")
+print("What is your name?")
+myName = input()
+print("It is good to meet you, ", myName)
+print("The length of your name is: ", len(myName))
+print("What is your age?")  # ask for their age
+myAge = input()
+print("You will be " + str(int(myAge)+1) + " in a year")
